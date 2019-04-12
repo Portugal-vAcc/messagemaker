a391f4dca733b1cfcec2b3b33c23118c7fa4f97b
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-04-12 14:05:35 +0100

    upgrade audio packaging to remove special chars
    required to upload to gng

--- manage.py
+++ manage.py
@@ -17,8 +17,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Message Maker.  If not, see <http://www.gnu.org/licenses/>.
 """
+import os
 import click
 import subprocess
+import shutil
+from pathlib import Path
 
 @click.group()
 def cli():
@@ -50,6 +53,34 @@ def test(only=None):
         suite.append(only)
     subprocess.call(suite)
     subprocess.call(['coverage', 'report', '--show-missing'])
+
+@cli.command()
+@click.argument('dest_folder')
+@click.option('--nuke', default=False, is_flag=True, help='clear only dest_folder, do not actually package audio files')
+def package(dest_folder, nuke):
+    try:
+        shutil.rmtree(dest_folder)
+    except FileNotFoundError:
+        pass
+    if nuke:
+        return
+
+    os.makedirs(dest_folder)
+    with open(f'{dest_folder}/atisfile.txt', 'w') as atisfile:
+        for file in os.listdir('audio'):
+            audio = Path(file)
+            if audio.suffix != '.wav':
+                continue
+
+            safe_filename = audio.stem.replace('.', '')
+            subprocess.call(['sox',
+                '--norm', f'audio/{file}',
+                '-b', '16',     # sample size, 16bits
+                '-c', '1',      # audio channels, mono
+                '-r', '7350',   # bitrate, 7350Mhz
+                f'{dest_folder}/{safe_filename}{audio.suffix}'
+            ])
+            atisfile.write(f'RECORD:{audio.stem}:{safe_filename}{audio.suffix}\n')
 
 if __name__ == '__main__':
     cli()
