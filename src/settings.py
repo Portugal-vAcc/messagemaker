f34a72314f983ef858d1c7dc71fe62b7d696928b
becas22 <benyreis@hotmail.com>
2020-04-16 00:38:48 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@
 Message Maker
 
 Copyright (C) 2018 - 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
-                           Bernardo Reis
+                           Bernardo Reis <bernardo.paranhos.reis@gmail.com>
                            Hugo Santos <hugogpsantos@gmail.com>
 
 This file is part of Message Maker.
